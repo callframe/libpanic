c91cfdf6994729dfeafde64b57ecb57e8d48361a
Fabrice <fabrice@schaub-dev.xyz>
2026-06-28 23:25:40 +0200

    adding the panic machinery

--- panic.c
+++ panic.c
@@ -1,1 +1,48 @@
 #include "panic.h"
+
+extern Panic_Handle_Fn _PANIC_HANDLER;
+
+_Thread_local struct Panic_Frame* _PANIC_FRAME = NULL;
+_Thread_local struct Panic_Info _PANIC_INFO = PANIC_INFO_UNINIT;
+
+void _panic_frame_push(struct Panic_Frame* frame)
+{
+  frame->previous = _PANIC_FRAME;
+  _PANIC_FRAME = frame;
+}
+
+void _panic_frame_pop(void) { _PANIC_FRAME = _PANIC_FRAME->previous; }
+
+_Noreturn void _panic_raise(struct Panic_Info info)
+{
+  _PANIC_INFO = info;
+
+  struct Panic_Frame* frame = _PANIC_FRAME;
+  while (frame != NULL)
+  {
+    switch (frame->kind)
+    {
+      case Panic_Frame_Kind_Guard:
+        _PANIC_FRAME = frame;
+        _panic_context_restore(&frame->context);  // Does not return
+        __builtin_unreachable();
+
+      case Panic_Frame_Kind_Catch:
+        if (frame->predicate == NULL || frame->predicate(&_PANIC_INFO))
+        {
+          _PANIC_FRAME = frame;
+          _panic_context_restore(&frame->context);  // Does not return
+        }
+        break;  // If predicate doesn't match we continue unwinding the stack
+
+      case Panic_Frame_Kind_Root:
+        goto fail;
+    }
+
+    frame = frame->previous;
+  }
+
+fail:
+  _PANIC_HANDLER(&_PANIC_INFO);
+  __builtin_unreachable();
+}
--- panic.h
+++ panic.h
@@ -1,7 +1,13 @@
 #pragma once
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
+
+/* Panic_Context is used to save the CPU state when a panic occurs
+ * and recover it when the panic is handled. It is architecture-specific and
+ * must be implemented for each supported architecture.
+ */
 
 struct Panic_Context
 {
@@ -29,4 +35,48 @@ struct Panic_Context
 };
 
 bool _panic_context_save(struct Panic_Context* context);
-void _panic_context_restore(struct Panic_Context* context);
+_Noreturn void _panic_context_restore(struct Panic_Context* context);
+
+/* The Panic_Frame describes a frame in the panic handling stack.
+ * Catch kind is used for frames that catch a downstream panic and handle it.
+ * Guard kind is used for frames that guard a section of code and will rethrow
+ * the panic if it occurs. Root kind is used for the root frame of the panic
+ * handling stack.
+ */
+
+struct Panic_Info;
+
+enum Panic_Frame_Kind
+{
+  Panic_Frame_Kind_Root,
+  Panic_Frame_Kind_Guard,
+  Panic_Frame_Kind_Catch,
+};
+
+typedef bool (*Panic_Frame_Predicate_Fn)(struct Panic_Info const* info);
+
+struct Panic_Frame
+{
+  enum Panic_Frame_Kind kind;
+  struct Panic_Frame* previous;
+  struct Panic_Context context;
+  Panic_Frame_Predicate_Fn predicate;
+};
+
+void _panic_frame_push(struct Panic_Frame* frame);
+void _panic_frame_pop(void);
+
+/* Panic_Info describes the information associated with a panic. */
+
+typedef void (*Panic_Handle_Fn)(const struct Panic_Info* info);
+
+struct Panic_Info
+{
+  size_t type;
+  uint8_t* data;
+  Panic_Handle_Fn handle;
+};
+
+#define PANIC_INFO_UNINIT {0, NULL, NULL}
+
+_Noreturn void _panic_raise(struct Panic_Info info);
